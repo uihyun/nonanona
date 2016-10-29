382b0063ee09eb0a9677fecacae4def427451ab8
uihyun <uihyunkei@gmail.com>
2016-10-29 10:34:58 +0900

    review에서 태그로 사람 검색시 null exception 처리.

--- app/src/main/java/com/nuums/nuums/model/user/NsUser.java
+++ app/src/main/java/com/nuums/nuums/model/user/NsUser.java
@@ -1,23 +1,16 @@
 package com.nuums.nuums.model.user;
 
-import android.text.TextUtils;
-
-
 import com.google.gson.Gson;
-import com.google.gson.annotations.SerializedName;
 import com.nuums.nuums.AppController;
-import com.nuums.nuums.model.misc.Bookmark;
 import com.nuums.nuums.model.misc.Comment;
 import com.nuums.nuums.model.misc.NanumInfo;
 import com.yongtrim.lib.ContextHelper;
 import com.yongtrim.lib.model.user.User;
-import com.yongtrim.lib.model.user.UserManager;
 
 import java.text.Collator;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -28,23 +21,21 @@ import java.util.List;
  */
 public class NsUser extends User {
 
-    public static NsUser getUser(String jsonString) {
-        Gson gson = AppController.getInstance().getGson();
-        NsUser user = gson.fromJson(jsonString, NsUser.class);
-        return user;
-    }
-
-
     public NsUser() {
 
         keywords = new ArrayList<>();
 
-        for(int i = 0;i < 9;i++) {
+        for (int i = 0; i < 9; i++) {
             keywords.add("");
         }
 
     }
 
+    public static NsUser getUser(String jsonString) {
+        Gson gson = AppController.getInstance().getGson();
+        NsUser user = gson.fromJson(jsonString, NsUser.class);
+        return user;
+    }
 
     public void patch(ContextHelper contextHelper) {
 //        List<NanumInfo> nanumsNew = new ArrayList<>();
@@ -60,9 +51,9 @@ public class NsUser extends User {
 
         List<NanumInfo> applysNew = new ArrayList<>();
 
-        if(applys != null) {
+        if (applys != null) {
             for (NanumInfo nanumInfo : applys) {
-                if(nanumInfo.isActive()) {
+                if (nanumInfo.isActive()) {
                     applysNew.add(nanumInfo);
                 }
             }
@@ -83,20 +74,22 @@ public class NsUser extends User {
     }
 
 
-    public  NsUserSearch[] getSearchList(ContextHelper contextHelper, String keyword, java.util.ArrayList<NsUser> userNetwork) {
+    public NsUserSearch[] getSearchList(ContextHelper contextHelper, String keyword, java.util.ArrayList<NsUser> userNetwork) {
         HashMap<String, NsUser> map = new HashMap<String, NsUser>();
 
         //HashSet hs = new HashSet();
-        for(NanumInfo nanumInfo : nanums) {
+        for (NanumInfo nanumInfo : nanums) {
 
-            for(Comment comment : nanumInfo.getNanum().getWons()) {
-                if(!comment.getOwner().isAdmin() && comment.getOwner().getNicknameSafe().contains(keyword))
-                    map.put(comment.getOwner().getUsername(), comment.getOwner());
+            if (nanumInfo.getNanum() != null) {
+                for (Comment comment : nanumInfo.getNanum().getWons()) {
+                    if (!comment.getOwner().isAdmin() && comment.getOwner().getNicknameSafe().contains(keyword))
+                        map.put(comment.getOwner().getUsername(), comment.getOwner());
+                }
             }
         }
 
-        for(NanumInfo nanumInfo : applys) {
-            if(!nanumInfo.getNanum().getOwner().isAdmin() && nanumInfo.getNanum().getOwner().getNicknameSafe().contains(keyword))
+        for (NanumInfo nanumInfo : applys) {
+            if (!nanumInfo.getNanum().getOwner().isAdmin() && nanumInfo.getNanum().getOwner().getNicknameSafe().contains(keyword))
                 map.put(nanumInfo.getNanum().getOwner().getUsername(), nanumInfo.getNanum().getOwner());
         }
 
@@ -121,27 +114,27 @@ public class NsUser extends User {
 
         ArrayList<NsUserSearch> arrResult = new ArrayList<>();
 
-        for(NsUser user : list) {
-            if(!user.isMe(contextHelper))
+        for (NsUser user : list) {
+            if (!user.isMe(contextHelper))
                 arrResult.add(new NsUserSearch(user.getNickname(), user));
         }
 
 
-        if(userNetwork == null)
+        if (userNetwork == null)
             arrResult.add(new NsUserSearch(keyword + "검색중...", null));
 
 
-        if(userNetwork != null) {
-            for(NsUser user : userNetwork) {
+        if (userNetwork != null) {
+            for (NsUser user : userNetwork) {
                 boolean has = false;
-                for(NsUser user2 : list) {
-                    if(user2.isSame(user)) {
+                for (NsUser user2 : list) {
+                    if (user2.isSame(user)) {
                         has = true;
                         break;
                     }
                 }
-                if(!has) {
-                    if(!user.isMe(contextHelper))
+                if (!has) {
+                    if (!user.isMe(contextHelper))
                         arrResult.add(new NsUserSearch(user.getNickname(), user));
                 }
             }
@@ -149,9 +142,7 @@ public class NsUser extends User {
         }
 
 
-
-
-        NsUserSearch []appArray = new NsUserSearch[arrResult.size()];
+        NsUserSearch[] appArray = new NsUserSearch[arrResult.size()];
         appArray = arrResult.toArray(appArray);
 
 
